4ecaa79aa93c31d53d5c107078418da12ed49c71
Adam Wong <adam.wong789@googlemail.com>
2024-11-20 00:44:19 +0000

    Added paddle rendering and movement. Overriding long-press delay

--- pong.ts
+++ pong.ts
@@ -1,10 +1,14 @@
 
 type gameRulesType = {
-    paddle_dimensions: [number, number],
-    ball_size: number,
-    ball_speed: number,
-    paddle_speed: number
+    paddleDimensions: [number, number],
+    ballSize: number,
+    ballSpeed: number,
+    paddleSpeed: number
 };
+
+const X = 0;
+const Y = 1;
+
 window.onload = () => {
     const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
     const canvasContext: CanvasRenderingContext2D | null = canvas.getContext('2d');
@@ -13,21 +17,62 @@ window.onload = () => {
     }
 
     let gameRules:gameRulesType = {
-        paddle_dimensions: [100, 10],
-        ball_size: 10,
-        ball_speed: 5,
-        paddle_speed: 5
+        paddleDimensions: [100, 10],
+        ballSize: 10,
+        ballSpeed: 5,
+        paddleSpeed: 5
     }
 
 
-    drawEverything(canvasContext, gameRules);
+    let ballPos: [number, number] = [0, 0];
+    let paddlePos: [number, number] = [canvas.width/2 - gameRules.paddleDimensions[X]/2, canvas.height - canvas.height/4];
+
+    let keyPressed: boolean = false;
+    let keyDirection: string = '';
+    const fps: number = 10;
+
+    window.addEventListener('keydown', (event) => {
+        if(!keyPressed){
+            if (event.key === 'ArrowLeft'){
+                keyDirection = 'ArrowLeft';
+            } else if (event.key === 'ArrowRight'){
+                keyDirection = 'ArrowRight';
+            }
+            keyPressed = true;
+        }
+    });
+
+    window.addEventListener('keyup', (event) => {
+        keyPressed  = false;
+        keyDirection = '';
+    });
+    setInterval(() => {
+        paddlePos[X] = movePaddle(keyDirection, paddlePos, gameRules.paddleSpeed);
+        drawEverything(canvasContext, gameRules, ballPos, paddlePos);
+    }, 1000/fps);
+    
 
 
 
 }
 
-function drawEverything(context: CanvasRenderingContext2D, gameRules: gameRulesType) {
-    console.log("drawEverything");
+function drawEverything(
+        context: CanvasRenderingContext2D,
+        gameRules: gameRulesType,
+        ballPos: [number, number],
+        paddlePos: [number, number]
+    ) {
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
     context.fillStyle = 'white';
-    context.fillRect(0, 0, gameRules.paddle_dimensions[0], gameRules.paddle_dimensions[1]);
+    context.fillRect(paddlePos[X], paddlePos[Y], gameRules.paddleDimensions[0], gameRules.paddleDimensions[1]);
+}
+
+
+function movePaddle(event: string, paddlePos: [number, number], speed: number) {
+    if (event === 'ArrowLeft') {
+        return paddlePos[X] - speed;
+    } else if (event === 'ArrowRight') {
+        return paddlePos[X] + speed;
+    } else return paddlePos[X];
 }
